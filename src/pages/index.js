c0ea2e32a56a14e37c49121e6227caf758a46078
Biraj Rai <70630924+kathmandhu@users.noreply.github.com>
2024-10-17 20:45:32 +0545

    fix:: centered the spinner

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,11 @@ import Cookies from 'js-cookie';
 import { usePlayerData } from '../hooks/usePlayerData';
 
 // Spinner Component
+// Spinner Component
 const Spinner = () => (
-    <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-yellow-400"></div>
+    <div className="flex items-center justify-center h-full">
+        <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-yellow-400"></div>
+    </div>
 );
 
 // Player Item Component
